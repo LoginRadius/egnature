18c8af4c95c667f6bf2a6559ba5b56fd1d26d1c5
HiteshKumawat <hitesh.kumawat@loginradius.com>
2020-10-25 15:36:00 +0530

    chore: introducing step3

--- src/App.js
+++ src/App.js
@@ -579,6 +579,43 @@ class App extends React.Component {
               </div>
             </div>
           </div>
+          {finalImage && (
+            <React.Fragment>
+              <div className="app-inner-wrap">
+                <div className="headText">
+                  <span>STEP 3</span>
+                  <p>Add your Generated Signature to Gmail</p>
+                </div>
+                <div class="signature-wrap gmail-steps">
+                  <ol>
+                    <li>
+                      Open Gmail in a web browser. Sign in. Open Gmail settings
+                      under the gear icon in the right top corner in Gmail.
+                    </li>
+                    <li>
+                      Scroll down to the signature field. If you have something
+                      inside signature field then delete everything inside of
+                      it; including spaces. Paste in the signature that was
+                      copied in the STEP 2 (right click into the input field and
+                      click "Paste" or use keyboard shortcut "Ctrl + V" after
+                      you clicked into the input field). Make sure the radio
+                      button that is selected in the Signature section of the
+                      settings page is NOT the one beside label "No signature".
+                    </li>
+                    <li>
+                      You should see your signature now in the signature field,
+                      check it over to make sure there are no typos. DO NOT
+                      attempt to fix ANYTHING inside Gmail. If there are any
+                      typos then go back to STEP 1 and regenerate the signature.
+                      Once you made sure everything is good, scroll down to the
+                      very bottom of the page and save your changes via the Save
+                      button.
+                    </li>
+                  </ol>
+                </div>
+              </div>
+            </React.Fragment>
+          )}
         </div>
       </div>
     );
